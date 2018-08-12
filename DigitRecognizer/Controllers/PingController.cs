9a8ade6fa3fa356962a3285d430a26049dc66dae
zabik <zaaabik@gmail.com>
2018-08-12 17:40:47 +0300

    initial commit

---
+++ DigitRecognizer/Controllers/PingController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DigitRecognizer.Controllers {
+    public class PingController : Controller {
+        [HttpGet("/ping")]
+        public string Ping() {
+            return "pong";
+        }
+    }
+}
---
+++ DigitRecognizer/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DigitRecognizer {
+    public class Startup {
+        public Startup(IHostingEnvironment env) {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath);                
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddMvc();
+            services.AddCors(co =>
+                co.AddPolicy("AllowAll", cp => cp.AllowAnyOrigin().WithMethods("GET").AllowAnyHeader()));
+        }
+        
+        public void Configure(IApplicationBuilder app, IServiceProvider serviceProvider) {
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
