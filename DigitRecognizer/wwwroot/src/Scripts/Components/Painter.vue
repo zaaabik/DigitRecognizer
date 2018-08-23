0a839fbda502a075c36478f2b6d9a7e00a9548b0
zabik <zaaabik@gmail.com>
2018-08-24 02:39:19 +0300

    добавил верстку для мобилки

--- DigitRecognizer/wwwroot/src/Scripts/Components/Painter.vue
+++ DigitRecognizer/wwwroot/src/Scripts/Components/Painter.vue
@@ -3,11 +3,13 @@
     >
         <div class="painterro"
              id="painterro"
+             :class="isMobile"
         ></div>
         <b-button v-if="!loadingPrediction"
                   color="primary"
+                  :class="isMobile"
                   type="button"
-                  class="mt-5 btn btn-primary"
+                  class="mt-5 btn btn-primary predict-button"
                   @click="saveImage"
         >
             Predict
@@ -18,20 +20,20 @@
         <b-modal title="Predict result"
                  ok-only
                  v-model="showDialog"
+                 class="predict-modal align-items-center"
+                 :class="isMobile"
         >
             <p>Predicted number: {{predictedNumber}}</p>
             <p>Predicted percent: {{predictedPercent}} %</p>
         </b-modal>
     </div>
 </template>
-
 <script>
     import Painterro from 'painterro';
     import {Image} from '@/Scripts/Common/api.js'
 
     export default {
         name: "MPAINTERRO",
-
         mounted() {
             this.showPainerro();
         },
@@ -53,14 +55,14 @@
                     defaultTool: 'brush',
                     availableLineWidths: [10],
                     defaultLineWidth: 10,
-                    fixMobilePageReloader: true,
+                    fixMobilePageReloader: false,
                     hiddenTools: ['crop', 'toolbar', 'weight', 'line', 'rect', 'ellipse', 'text', 'rotate', 'resize', 'save', 'open', 'close', 'settings'],
                     saveHandler: function (image, done) {
                         _this.loadingPrediction = true;
                         Image().predict(image).then((response) => {
                             let data = response.data;
                             _this.predictedNumber = data.number;
-                            _this.predictedPercent = data.probability;
+                            _this.predictedPercent = Math.round(data.probability * 100) / 100;
                             done();
                             _this.showDialog = true;
                         }).catch(() => {
@@ -75,7 +77,15 @@
             saveImage() {
                 this.Painterro.save();
             }
+        },
+        computed: {
+            isMobile() {
+                return /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(
+                    navigator.userAgent) ? 'mobile' : '';
+
+            }
         }
+
     }
 </script>
 
